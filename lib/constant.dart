622ce8369c2d74949f009b80bd51b9b65291e8a5
sreeraj <sreerajs728@gmail.com>
2022-09-09 17:55:45 +0530

    login page design completed

--- lib/constant.dart
+++ lib/constant.dart
@@ -1,7 +1,46 @@
 //Routes
+import 'package:flutter/material.dart';
+import 'package:flutter/rendering.dart';
+
 const baseUrl = "http://localhost:8000/api";
 const loginUrl = "$baseUrl/login";
 const registerUrl = "$baseUrl/register";
 const userUrl = "$baseUrl/get_user";
 //Errors
 const serviceError = "Server Error";
+
+
+// DECORATION FUNCTION
+InputDecoration textInputDecor(String label){
+  return InputDecoration(
+      labelText: label,
+      contentPadding: const EdgeInsets.all(10),
+      border: const OutlineInputBorder(borderSide: BorderSide(width: 1, ))
+  );
+} 
+
+// BUTTON STYLING
+ElevatedButton srButton(String label, Function onPressed){
+  return ElevatedButton(
+      style: ButtonStyle(
+        backgroundColor: MaterialStateColor.resolveWith((states) => Colors.blue),
+        padding: MaterialStateProperty.resolveWith((states) => const EdgeInsets.symmetric(vertical: 10))
+      ),
+      onPressed: ()=> onPressed(),
+      child: Text(label, style: const TextStyle(color: Colors.white),), 
+    );
+}
+
+// LOGIN REGISTER HINT
+Row srLogRegHint(String text, String label, Function onTap){
+  return Row(
+    mainAxisAlignment: MainAxisAlignment.center,
+    children: [
+    Text(text),
+    GestureDetector(
+      child: Text(label, style: const TextStyle(color: Colors.blue)),
+      onTap: () => onTap()
+    )
+  ],
+  );
+}
--- lib/screens/login.dart
+++ lib/screens/login.dart
@@ -1,6 +1,7 @@
-import 'package:flutter/src/foundation/key.dart';
-import 'package:flutter/src/widgets/framework.dart';
 import 'package:flutter/material.dart';
+
+import '../constant.dart';
+import 'register.dart';
 
 class Login extends StatefulWidget {
   const Login({Key? key}) : super(key: key);
@@ -10,12 +11,46 @@ class Login extends StatefulWidget {
 }
 
 class _LoginState extends State<Login> {
+  final GlobalKey<FormState> formkey = GlobalKey<FormState>();
+  TextEditingController txtEmail = TextEditingController();
+  TextEditingController password = TextEditingController();
   @override
   Widget build(BuildContext context) {
     return Scaffold(
       appBar: AppBar(
         title: const Text('Login'),
         centerTitle: true,
+      ),
+      body: Form(
+        key: formkey,
+        child: ListView(
+          padding: const EdgeInsets.all(30),
+          children:[
+            TextFormField(
+              keyboardType: TextInputType.emailAddress,
+              controller: txtEmail,
+              validator: (val)=> val!.isEmpty ? "Invalid Eamil Address": null,
+              decoration: textInputDecor('Email')
+            ),
+            const SizedBox(height: 10,),
+            TextFormField(
+              obscureText: true,
+              controller: password,
+              validator: (val)=> val!.isEmpty ? "Required atleast 4 characters": null,
+              decoration: textInputDecor('Password')
+            ),
+            const SizedBox(height: 10,),
+            srButton('Login', (){
+              if(formkey.currentState!.validate()){
+                
+              }
+            }),
+            const SizedBox(height: 10,),
+            srLogRegHint("Don't have an account?", " Register", (){
+              Navigator.of(context).pushAndRemoveUntil(MaterialPageRoute(builder: (context)=>register()), (route)=>false);
+            })
+          ]
+        )
       ),
     );
   }
